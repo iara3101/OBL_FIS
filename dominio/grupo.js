f468619643dadaa2cbaf9af2bfc39e7551ff0b9a
Mateo Arias <mateoarias437@gmail.com>
2022-11-16 15:54:53 -0300

    Agrgar usuaio a grupo

--- dominio/grupo.js
+++ dominio/grupo.js
@@ -5,7 +5,7 @@ export default class grupo{
         this.idGrupo = idUnGrupo;
         this.listaIntegrantes=[];
         this.tipoGrupo = unTipoGrupo;
-        this.tipo = unTipo; //si es true es amistoso, si es false es por dinero
+        this.tipoApuesta = unTipo; //si es true es amistoso, si es false es por dinero
         this.listaApuestas=[];
         this.fecha=unaFecha;
         this.admin = unUsuario;
